212bce5b9a40f22669c5659e125a8a9d0e2df95a
ljocbaniadev <laureenocbaniadev@gmail.com>
2026-08-10 01:06:43 +0800

    Add PostgreSQL service for backend CI

--- personal-library-backend/src/test/java/com/personallibrary/personal_library_backend/PersonalLibraryBackendApplicationTests.java
+++ personal-library-backend/src/test/java/com/personallibrary/personal_library_backend/PersonalLibraryBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.personallibrary.personal_library_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PersonalLibraryBackendApplicationTests {
 
 	@Test
